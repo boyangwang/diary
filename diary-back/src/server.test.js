6f2791dee91c2e996736a92f5c57a974d05fd6ae
boyang <wangboyang1991@gmail.com>
2017-09-23 15:09:35 +0800

    more opts to helper method

--- diary-back/src/server.test.js
+++ diary-back/src/server.test.js
@@ -6,49 +6,61 @@ const mongoUrl = `mongodb://localhost:27017/${dbName}`;
 let appInstance, db;
 
 beforeAll(async () => {
-    db = await MongoClient.connect(mongoUrl);
-    appInstance = await require('./server.js')({dbName});
+  db = await MongoClient.connect(mongoUrl);
+  appInstance = await require('./server.js')({dbName});
 });
 
-let expectFetchUrlStatusCodeAndJson = async (url, expectStatusCode, expectJson) => {
-  let response = await fetch(url);
+let expectFetchUrlStatusCodeAndJson = async ({url, expectStatusCode, expectJson,
+    method, postBody}) => {
+  let response;
+  if (!method)
+    response = await fetch(url);
+  else if (method === 'POST') {
+    response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method,
+      body: JSON.stringify(postBody)
+    });
+  }
   expect(response.status).toBe(expectStatusCode);
   let body = await response.json();
   expect(body).toEqual(expectJson);  
 }
 
 test('/api/getEntriesForDate require date and owner params', async () => {
-  await expectFetchUrlStatusCodeAndJson(
+  await expectFetchUrlStatusCodeAndJson({url:
     `http://localhost:${config.port}/api/getEntriesForDate`,
-    400, {err: 'Missing query param'});
+    expectStatusCode: 400, expectJson: {err: 'Missing query param'}});
 
-  await expectFetchUrlStatusCodeAndJson(
+  await expectFetchUrlStatusCodeAndJson({url:
     `http://localhost:${config.port}/api/getEntriesForDate?owner=testOwner`,
-    400, {err: 'Missing query param'});
+    expectStatusCode: 400, expectJson: {err: 'Missing query param'}});
   
-  await expectFetchUrlStatusCodeAndJson(
+  await expectFetchUrlStatusCodeAndJson({url:
     `http://localhost:${config.port}/api/getEntriesForDate?date=1970-01-01`,
-    400, {err: 'Missing query param'});
+    expectStatusCode: 400, expectJson: {err: 'Missing query param'}});
 });
 
 test('/api/getEntriesForDate require date and owner params legal', async () => {
-  await expectFetchUrlStatusCodeAndJson(
+  await expectFetchUrlStatusCodeAndJson({url:
     `http://localhost:${config.port}/api/getEntriesForDate?owner=_admin&date=1970-01-01`,
-    400, {err: 'Illegal query param'});
+    expectStatusCode: 400, expectJson: {err: 'Illegal query param'}});
 
-  await expectFetchUrlStatusCodeAndJson(
+  await expectFetchUrlStatusCodeAndJson({url:
     `http://localhost:${config.port}/api/getEntriesForDate?owner=testOwner&date=1234`,
-    400, {err: 'Illegal query param'});
+    expectStatusCode: 400, expectJson: {err: 'Illegal query param'}});
 });
 
 test('/api/getEntriesForDate returns a entry', async () => {
-    let testOwnerEntryCollection = db.collection(`entry_testOwner`);
-    let entry = {_id: 'testid', date: "1970-01-01", title:
-      "test title", content: "test content", points: 1};
-    await testOwnerEntryCollection.insertOne(entry);
-    await expectFetchUrlStatusCodeAndJson(
-      `http://localhost:${config.port}/api/getEntriesForDate?date=1970-01-01&owner=testOwner`,
-      200, {data: [entry]});
+  let testOwnerEntryCollection = db.collection(`entry_testOwner`);
+  let entry = {_id: 'testid', date: "1970-01-01", title:
+    "test title", content: "test content", points: 1};
+  await testOwnerEntryCollection.insertOne(entry);
+  await expectFetchUrlStatusCodeAndJson({url:
+    `http://localhost:${config.port}/api/getEntriesForDate?date=1970-01-01&owner=testOwner`,
+    expectStatusCode: 200, expectJson: {data: [entry]}});
 });
 
 test('/api/postEntryForDate adds a entry', async () => {
