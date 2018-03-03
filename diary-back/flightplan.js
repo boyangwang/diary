bfcc90a2df61ddb8f6a7197d7932226e2f5d7373
Boyang <wangboyang1991@gmail.com>
2018-03-04 03:32:26 +0800

    refactored flightplan using exec and with

--- diary-back/flightplan.js
+++ diary-back/flightplan.js
@@ -17,24 +17,28 @@ const projectsDir = '/var/www/diary_project';
 plan.remote(['deploy'], (remote) => {
   // download and unzip
   remote.exec(`mkdir ${projectsDir}`, { failsafe: true });
-  remote.exec(`cd ${projectsDir} &&
-    wget https://github.com/boyangwang/diary/archive/master.zip -O master.zip &&
-    rm -rf ./diary-master/ && 7z x master.zip -y`);
+  remote.with(`cd ${projectsDir}`, () => {
+    remote.exec(`wget https://github.com/boyangwang/diary/archive/master.zip -O master.zip`);
+    remote.exec(`rm -rf ./diary-master/ && 7z x master.zip -y`);
+    remote.exec(`chmod -R +X .`);
+  });
   // install and build
   // back
+  // run mongod if not running
   remote.exec(`nohup mongod --bind_ip 127.0.0.1 --dbpath ${projectsDir}/diary-master/diary-back/mongo/data &`, { failsafe: true });
-  remote.exec(`cd ${projectsDir}/diary-master/diary-back &&
-    yarn install --ignore-engines && ./node_modules/.bin/pm2 stop diary-back
-    ./node_modules/.bin/pm2 start ./src/server.js --name diary-back --interpreter=$(which node)`);
+  remote.with(`cd ${projectsDir}/diary-master/diary-back`, () => {
+    remote.exec(`yarn install --ignore-engines`);
+    remote.exec(`./node_modules/.bin/pm2 stop diary-back`, { failsafe: true });
+    remote.exec(`./node_modules/.bin/pm2 start ./src/server.js --name diary-back --interpreter=$(which node)`);
+  })
   // front
-  remote.exec(`cd ${projectsDir}/diary-master/diary-front &&
-    yarn install --ignore-engines && yarn run build`);
-  remote.exec(`cd ${projectsDir}/diary-master/diary-front &&
-    ln -sf ${projectsDir}/diary-master/diary-front/config/diary.conf /etc/nginx/sites-enabled/`);
-  remote.exec(`cd ${projectsDir} && chmod -R +X .`);
-  remote.exec(`cd ${projectsDir}/diary-master/diary-front &&
-    chmod -R 755 ./build`);
-  remote.exec(`nginx -s reload`);
+  remote.with(`cd ${projectsDir}/diary-master/diary-front`, () => {
+    remote.exec(`yarn install --ignore-engines`);
+    remote.exec(`yarn run build`);
+    remote.exec(`ln -sf ${projectsDir}/diary-master/diary-front/config/diary.conf /etc/nginx/sites-enabled/`);
+    remote.exec(`chmod -R 755 ./build`);
+    remote.exec(`nginx -s reload`);
+  });
 });
 
 plan.remote(['start'], (remote) => {
