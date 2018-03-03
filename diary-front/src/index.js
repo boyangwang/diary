3abe469058a4a62a73ff83caccb6582524e9e3a3
Boyang <wangboyang1991@gmail.com>
2018-03-03 17:44:35 +0800

    reflect frontend ver

--- diary-front/src/index.js
+++ diary-front/src/index.js
@@ -6,6 +6,7 @@ import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import reducer from './reducers'
 import Diary from './containers/Diary';
+import api from './utils/api';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
---
+++ diary-front/src/utils/api.js
@@ -0,0 +1,7 @@
+const packagejson = require('./package.json');
+
+console.info('diary-front ', packagejson);
+
+export default {
+
+};
