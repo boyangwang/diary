b119f473f448593567e8323638cc9ca45840ff5c
Boyang <wangboyang1991@gmail.com>
2018-03-06 16:14:53 +0800

    move common methods to testutils

---
+++ diary-back/src/testutils.js
@@ -0,0 +1,37 @@
+require('isomorphic-fetch');
+
+module.exports = {
+  expectFetchUrlStatusCodeAndJson: async ({ url, expectStatusCode, expectJson,
+    method, postBody }) => {
+    let response;
+    if (!method)
+      response = await fetch(url);
+    else if (method === 'POST') {
+      response = await fetch(url, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method,
+        body: JSON.stringify(postBody)
+      });
+    }
+    expect(response.status).toBe(expectStatusCode);
+    let body = await response.json();
+    if (expectJson)
+      expect(body).toEqual(expectJson);
+    return body;
+  },
+  expectDbQueryResult: async ({ collection, query, expectedResults }) => {
+    if (typeof collection === 'string')
+      collection = db.collection(collection);
+    let dbResult = await (await collection.find(query)).toArray();
+    dbResult = dbResult.map(document => {
+      if (typeof document._id !== 'string') {
+        return Object.assign({}, document, { _id: document._id.toString() });
+      }
+      return document;
+    });
+    expect(dbResult).toEqual(expectedResults);
+    return dbResult;
+  },
+};
