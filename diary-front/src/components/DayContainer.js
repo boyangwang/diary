28647bb9272d8934e66fb53b352b4f8b9f3b262e
Boyang <wangboyang1991@gmail.com>
2018-03-07 02:53:00 +0800

    moved date container

--- diary-front/src/components/DayContainer.js
+++ diary-front/src/components/DayContainer.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class DayContainer extends React.Component {
-    componentWillMount() {
-
-    }
-
     render() {
         return <div>{this.props.date}</div>
     }
