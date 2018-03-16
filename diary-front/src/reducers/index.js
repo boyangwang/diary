4838f137bb6896ede54322bb345f2e0eddcd6ffd
Boyang <wangboyang1991@gmail.com>
2018-03-16 17:52:40 +0800

    pretty

--- diary-front/src/reducers/index.js
+++ diary-front/src/reducers/index.js
@@ -8,7 +8,8 @@ export default (state = INITIAL_STATE, action) => {
   if (action.type === 'LOGIN') {
     return { ...state, user: action.payload.user };
   } else if (action.type === 'VERSION') {
-    return { ...state,
+    return {
+      ...state,
       backendVersion: action.payload.backendVersion ||
         state.backendVersion,
       frontendVersion: action.payload.frontendVersion ||
@@ -26,30 +27,36 @@ export default (state = INITIAL_STATE, action) => {
     newEntriesArr.push(action.payload.entry);
     return {
       ...state,
-      entriesDateMap: { ...state.entriesDateMap,
-        [date]: newEntriesArr },
+      entriesDateMap: {
+        ...state.entriesDateMap,
+        [date]: newEntriesArr
+      },
     };
   } else if (action.type === 'UPDATE_ENTRY') {
     const date = action.payload.entry.date;
     let newEntriesArr = _.isArray(state.entriesDateMap[date]) ?
       state.entriesDateMap[date].slice() : [];
     const findIndex = newEntriesArr.findIndex(entry => entry._id == action.payload.entry._id);
-    newEntriesArr = [...newEntriesArr.slice(0, findIndex), action.payload.entry, ...newEntriesArr.slice(findIndex+1)];
+    newEntriesArr = [...newEntriesArr.slice(0, findIndex), action.payload.entry, ...newEntriesArr.slice(findIndex + 1)];
     return {
       ...state,
-      entriesDateMap: { ...state.entriesDateMap,
-        [date]: newEntriesArr },
+      entriesDateMap: {
+        ...state.entriesDateMap,
+        [date]: newEntriesArr
+      },
     };
   } else if (action.type === 'DELETE_ENTRY') {
     const date = action.payload.entry.date;
     let newEntriesArr = _.isArray(state.entriesDateMap[date]) ?
       state.entriesDateMap[date].slice() : [];
     const findIndex = newEntriesArr.findIndex(entry => entry._id == action.payload.entry._id);
-    newEntriesArr = [...newEntriesArr.slice(0, findIndex), ...newEntriesArr.slice(findIndex+1)];
+    newEntriesArr = [...newEntriesArr.slice(0, findIndex), ...newEntriesArr.slice(findIndex + 1)];
     return {
       ...state,
-      entriesDateMap: { ...state.entriesDateMap,
-        [date]: newEntriesArr },
+      entriesDateMap: {
+        ...state.entriesDateMap,
+        [date]: newEntriesArr
+      },
     };
   } else if (action.type === 'TODOS') {
     return {
@@ -59,7 +66,7 @@ export default (state = INITIAL_STATE, action) => {
   } else if (action.type === 'POST_TODO') {
     return {
       ...state,
-      todos: [ ...state.todos, action.payload.todo ],
+      todos: [...state.todos, action.payload.todo],
     };
   } else {
     return state;
