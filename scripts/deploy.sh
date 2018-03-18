e8a92de317897d09d1b395d89323c1ea923ca8ef
Boyang <wangboyang1991@gmail.com>
2018-03-19 01:40:11 +0800

    DEPLOY: increment version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 cd ../diary-front && yarn version --new-version patch && \
 cd ../diary-back && yarn config set version-git-tag false && \
 yarn version --new-version patch && yarn config set version-git-tag true && \
-cd .. && git commit -am "DEPLOY: increment version" && git push origin master && \
+cd .. && git commit -am "DEPLOY: increment version" && git push --tags origin master && \
 cd diary-back && export FLIGHTPLAN_KEY_PATH=~/.ssh/id_rsa && \
 fly deploy-all:staging --flightplan ./flightplan.js
