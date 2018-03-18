0d0eaa876417bdf9717091fa058ab80dee2b5797
Boyang <wangboyang1991@gmail.com>
2018-03-18 11:26:49 +0800

    add classnames declaration

--- diary-front/src/types.d.ts
+++ diary-front/src/types.d.ts
@@ -1,3 +1,7 @@
 declare module 'append-query' {
   export default function appendQuery(url: string, params: object): string;
 }
+
+declare module 'classnames' {
+  export default function classnames(): string;
+}
