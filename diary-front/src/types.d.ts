6f3051da669eb41af11542c9130f8cfa41caf277
Boyang <wangboyang1991@gmail.com>
2018-03-21 02:14:03 +0800

    appendquery type - need to use options now

--- diary-front/src/types.d.ts
+++ diary-front/src/types.d.ts
@@ -1,5 +1,14 @@
+declare class AppendQueryOptions {
+  encodeComponents?: boolean;
+  removeNull?: boolean;
+}
+
 declare module 'append-query' {
-  export default function appendQuery(url: string, params: object): string;
+  export default function appendQuery(
+    url: string,
+    params: object,
+    options?: AppendQueryOptions
+  ): string;
 }
 
 declare module 'classnames' {
