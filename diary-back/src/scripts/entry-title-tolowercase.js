be195824a966388e2f9534637f8332d3d459c264
Boyang <wangboyang1991@gmail.com>
2018-03-21 22:49:21 +0800

    fix back package.json for deploy.sh change

--- diary-back/src/scripts/entry-title-tolowercase.js
+++ diary-back/src/scripts/entry-title-tolowercase.js
@@ -9,10 +9,10 @@ const mongoUrl = `mongodb://localhost:27017/diary`;
 
   let results = await (await ownerEntryCollection.find({})).toArray();
 
-  console.log('results', results.map(r => r.title));
+  console.log('results', results.map((r) => r.title));
 
   let res;
-  results.forEach(async doc => {
+  results.forEach(async (doc) => {
     res = await ownerEntryCollection.updateOne(
       { _id: ObjectId(doc._id) },
       { $set: { title: doc.title.toLocaleLowerCase() } }
