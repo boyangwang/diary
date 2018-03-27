10add7bcf62247ea9262889c60b0409523a06ddd
Boyang <wangboyang1991@gmail.com>
2018-03-27 15:38:08 +0800

    create diaryheadercontainer

--- diary-front/src/components/DiaryApp.tsx
+++ diary-front/src/components/DiaryApp.tsx
@@ -1,21 +1,21 @@
 import './DiaryApp.css';
 
-import { Button, Layout, Menu, message } from 'antd';
+import { Layout, message } from 'antd';
 import React from 'react';
 import { connect } from 'react-redux';
 
+import DiaryLoginView from 'components/DiaryLoginView';
 import DigestView from 'components/DigestModule/DigestView';
 import EntryView from 'components/EntryModule/EntryView';
-import DiaryLoginView from 'components/DiaryLoginView';
 import TodoView from 'components/TodoModule/TodoView';
 import { ReduxState, User } from 'reducers';
 import { dispatch } from 'reducers/store';
 import api, { ApiTestResponse, ErrResponse } from 'utils/api';
 import mylog from 'utils/mylog';
+import DiaryHeaderContainer from 'components/DiaryHeaderContainer';
 
 class ReduxProps {
   public user: User | null;
-  public backendVersion: null | string;
 }
 class DiaryApp extends React.Component<ReduxProps> {
   public componentWillMount() {
@@ -41,45 +41,13 @@ class DiaryApp extends React.Component<ReduxProps> {
     );
   }
 
-  public logout() {
-    api.logout().then(() => {
-      dispatch({
-        type: 'LOGOUT',
-        payload: { user: null },
-      });
-    });
-  }
-
   public render() {
-    const { user, backendVersion } = this.props;
+    const { user } = this.props;
     return (
       <div className="DiaryApp">
         <Layout>
           <Layout.Header>
-            <div className="DiaryAppTitleDiv logo">
-              <h1>DiaryApp</h1>
-              <h4 className="grey">{backendVersion}</h4>
-            </div>
-            <Menu theme="light" mode="horizontal">
-              <Menu.Item key="sync">
-                <Button onClick={() => window.location.reload()}>Sync</Button>
-              </Menu.Item>
-              <Menu.Item key="issue">
-                <Button>
-                  <a
-                    target="_blank"
-                    href="https://github.com/boyangwang/diary/issues/new"
-                  >
-                    Issue
-                  </a>
-                </Button>
-              </Menu.Item>
-              {user && (
-                <Menu.Item key="logout">
-                  <Button onClick={this.logout}>Logout</Button>
-                </Menu.Item>
-              )}
-            </Menu>
+            <DiaryHeaderContainer />
           </Layout.Header>
           <Layout.Content>
             {user ? (
@@ -101,6 +69,5 @@ class DiaryApp extends React.Component<ReduxProps> {
 export default connect((state: ReduxState) => {
   return {
     user: state.user,
-    backendVersion: state.backendVersion,
   };
 })(DiaryApp);
---
+++ diary-front/src/components/DiaryHeaderContainer.tsx
@@ -0,0 +1,61 @@
+import { connect } from "react-redux";
+import React from "react";
+import { Menu, Button } from "antd";
+
+import { ReduxState, User } from "reducers";
+import api from "utils/api";
+import { dispatch } from "reducers/store";
+
+class ReduxProps {
+  public user: User | null;
+  public backendVersion: null | string;
+}
+class DiaryHeaderContainer extends React.Component<ReduxProps> {
+  public logout() {
+    api.logout().then(() => {
+      dispatch({
+        type: 'LOGOUT',
+        payload: { user: null },
+      });
+    });
+  }
+  
+  public render() {
+    const { user, backendVersion } = this.props;
+    return (
+      <div>
+        <div className="DiaryAppTitleDiv logo">
+          <h1>DiaryApp</h1>
+          <h4 className="grey">{backendVersion}</h4>
+        </div>
+        <Menu theme="light" mode="horizontal">
+          <Menu.Item key="sync">
+            <Button onClick={() => window.location.reload()}>Sync</Button>
+          </Menu.Item>
+          <Menu.Item key="issue">
+            <Button>
+              <a
+                target="_blank"
+                href="https://github.com/boyangwang/diary/issues/new"
+              >
+                Issue
+                  </a>
+            </Button>
+          </Menu.Item>
+          {user && (
+            <Menu.Item key="logout">
+              <Button onClick={this.logout}>Logout</Button>
+            </Menu.Item>
+          )}
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default connect((state: ReduxState) => {
+  return {
+    user: state.user,
+    backendVersion: state.backendVersion,
+  };
+})(DiaryHeaderContainer);
