4f2942038741c2554e06cddec66f4f12850aece1
Boyang <wangboyang1991@gmail.com>
2018-04-23 17:25:15 +0800

    WIP: fetch

--- diary-front/src/utils/fetch.ts
+++ diary-front/src/utils/fetch.ts
@@ -1,5 +1,42 @@
 import 'isomorphic-fetch';
 
+import { notification } from 'antd';
+
+notification.config({
+  placement: 'bottomRight',
+  bottom: 40,
+  duration: 0,
+});
+let counter = 0;
+
 export default (url: string, opt: any) => {
-  return fetch(url, opt)
-}
+  const key = 'xhr-notification-' + counter++;
+  notification.open({
+    key,
+    message: 'URL: ' + url.replace(/$https?:\/\/[^/]+/, ''),
+    description: 'waiting...',
+  });
+
+  return fetch(url, opt).then(
+    (res) => {
+      notification.close(key);
+      if (!res.ok) {
+        notification.warning({
+          key,
+          message: 'URL: ' + url.replace(/$https?:\/\/[^/]+/, ''),
+          description: 'Error: ' + res.statusText,
+        });
+      }
+      return res;
+    },
+    (err) => {
+      notification.close(key);
+      notification.warning({
+        key,
+        message: 'URL: ' + url.replace(/$https?:\/\/[^/]+/, ''),
+        description: 'Error: ' + err,
+      });
+      throw err;
+    }
+  );
+};
